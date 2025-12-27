07de2de346ce5d9f552f014f5cb17bcd168e8530
Kevin Aud <kevinaud@gmail.com>
2025-12-27 16:18:08 +0000

    fix github cli auth

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,3 @@
 #! /bin/bash
 
 bash .devcontainer/setup-env.sh
-
-# Playwright browsers are pre-installed in the container image
-# No need to run playwright install here
-
-if [ -s /tmp/.gh_token_file ]; then
-  cat /tmp/.gh_token_file | gh auth login --with-token
-else
-  echo 'WARN: No GitHub token found. Skipping auto-login.'
-fi
